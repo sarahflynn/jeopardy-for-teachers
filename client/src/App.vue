63098c5bcb8bea6b32919d519bc6aa9664f18880
Sarah Flynn <sarah.flynn@icloud.com>
2019-01-24 14:37:25 -0800

    changed default font, header styling, nav styling, nav links

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
-    <p id="logo">Jeopardy!</p>
     <header>
-      <a v-if="user" href="/">Home</a>
-      <RouterLink v-if="!user" to="/auth">Sign in</RouterLink>
-      <a v-if="user" href="/" @click.prevent="handleSignOut">Sign Out</a>
-      <router-link to="/board" v-if="user"></router-link>
-      <router-link v-if="user" to="/"></router-link>
-      <span class="user" v-if="user">user: {{ user.name }}</span> 
+      <h1 id="logo">JEOPARDY FOR TEACHERS</h1>
     </header>
+
+    <nav>
+      <RouterLink v-if="user" class="navlink" to="/">My Dashboard</RouterLink>
+      <RouterLink v-if="user" class="navlink" to="/" @click.prevent="handleSignOut">My Dashboard</RouterLink>
+      <RouterLink v-if="!user" class="navlink" to="/auth">Sign in</RouterLink>
+      <RouterLink class="navlink" to="/about">About</RouterLink>
+      <RouterLink class="navlink" to="/instructions">How to Play</RouterLink>
+      <RouterLink class="navlink" to="/contact">Contact</RouterLink>
+    </nav>
+
+    <span class="user" v-if="user">Hello {{ user.name }}!</span>
+
     <RouterView class="routes" :onUser="handleUser" :user="user"></RouterView>
   </div>
 </template>
 
 <script>
-
 import { checkForToken, signOut } from './services/api';
 
 export default {
   name: 'app',
-  components: {
-  },
+  components: {},
   data() {
     return {
       user: null
@@ -43,26 +47,62 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Anton|Arimo:700|Crimson+Text|Fascinate|Montserrat');
 
 * {
   margin: 0;
-  font-family: 'Courier New', Courier, monospace;
+}
+
+header {
+  background-color: rgb(6, 11, 141);
 }
 
 #logo {
   text-align: center;
   padding-top: 3%;
   padding-bottom: 3%;
+  font-family: 'Fascinate', sans-serif;
   font-size: 4em;
   font-weight: bold;
+  color: rgba(255, 255, 255);
 }
 
 h1 {
   text-align: center;
-  margin: 5% 0 2% 0;
 }
 
-header {
+nav {
+    background-color: rgba(6, 14, 233, 0.52);
+    width: 100vw;
+    display: flex;
+    z-index: 999;
+}
+
+nav .navlink {
+    flex-grow: 1;
+    display: inline-block;
+    text-align: center;
+    font-size: 18px;
+    color: white;
+    line-height: 60px;
+    flex-direction: row;
+    width: 25vw;
+    transition: background-color 1s ease;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    font-family: 'Montserrat', serif;
+    font-weight: 600;
+}
+
+nav .navlink:hover {
+    background-color: rgb(163, 166, 243);
+    color: rgba(6, 14, 233, 0.);
+}
+
+
+
+/* nav {
     margin: 0;
     padding: 10px 10px 10px 10px;
     width: 100%;
@@ -72,15 +112,15 @@ header {
     text-align: center
 }
 
-header a {
+nav a {
     color: white;
     padding: 8px 16px;
     text-decoration: none;
 }
 
-header a:hover {
+nav a:hover {
     background-color: rgb(82, 137, 255);
-}
+} */
 
 .user {
   text-align: left;
