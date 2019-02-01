132eae027030e3b7d6783dfe500a2c7e5f8926d8
Sarah Flynn <sarah.flynn@icloud.com>
2019-02-01 11:52:08 -0800

    progress on mobile nav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
-    <!-- <nav>
-      <RouterLink v-if="user" class="navlink" to="/">My Dashboard</RouterLink>
-      <RouterLink class="navlink" to="/how-to-play">How to Play</RouterLink>
-      <RouterLink class="navlink" to="/about">About</RouterLink>
-      <a v-if="user" class="navlink" href="/" @click.prevent="handleSignOut">Sign Out</a>
-    </nav> -->
+    <nav>
+      <div class="icon">
+        <input id="hamburger" type="checkbox" v-model="checkboxToggle">
+        <label for="hamburger" class="hamburger"></label>
+      </div>
+      <div v-if="checkboxToggle && (width < 600)" class="navbar" id="navbar">
+        <RouterLink v-if="!user" class="navlink" to="/auth">Sign in</RouterLink>
+        <RouterLink v-if="user" class="navlink" to="/">My Dashboard</RouterLink>
+        <RouterLink class="navlink" to="/how-to-play">How to Play</RouterLink>
+        <RouterLink class="navlink" to="/about">About</RouterLink>
+        <a v-if="user" class="navlink" href="/" @click.prevent="handleSignOut">Sign Out</a>
+      </div>
+    </nav>
 
     <RouterView id="app-view" class="routes" :onUser="handleUser" :user="user"></RouterView>
   </div>
 </template>
 
 <script>
-import { checkForToken, signOut } from './services/api';
+import { checkForToken, signOut } from "./services/api";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {},
   data() {
     return {
-      user: null
+      user: null,
+      checkboxToggle: false,
+      width: null
     };
   },
   methods: {
@@ -29,53 +38,55 @@ export default {
     handleSignOut() {
       signOut();
       this.user = null;
-      this.$router.push('/auth');
+      this.$router.push("/auth");
     }
   },
   created() {
     this.user = checkForToken();
     if(!this.user) {
-      this.$router.push('/auth');
+      this.$router.push("/auth");
     }
+    this.width = window.innerWidth;
   }
 };
 </script>
 
 <style scoped>
 @import "../css/variables.css";
-@import url('https://fonts.googleapis.com/css?family=Anton|Arimo:700|Crimson+Text|Fascinate|Montserrat');
+@import url("https://fonts.googleapis.com/css?family=Anton|Arimo:700|Crimson+Text|Fascinate|Montserrat");
 
 h1 {
   text-align: center;
 }
 
 nav {
-    background-color: var(--theme0);
-    width: 100vw;
-    display: flex;
-    z-index: 999;
+  width: 100vw;
+  display: flex;
+  z-index: 999;
+  position: fixed;
+  top: 0;
 }
 
 nav .navlink {
-    flex-grow: 1;
-    display: inline-block;
-    text-align: center;
-    font-size: 18px;
-    color: white;
-    line-height: 60px;
-    flex-direction: row;
-    width: 25vw;
-    transition: background-color 1s ease;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-    font-family: 'Montserrat', serif;
-    font-weight: 600;
+  flex-grow: 2;
+  display: inline-block;
+  text-align: center;
+  font-size: 4.5vw;
+  color: white;
+  line-height: 60px;
+  flex-direction: row;
+  width: 25vw;
+  transition: background-color 1s ease;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  font-family: "Montserrat", serif;
+  font-weight: 600;
 }
 
 nav .navlink:hover {
-    background-color: var(--theme1);
-    color: rgba(6, 14, 233, 0.);
+  background-color: var(--theme1);
+  color: rgba(6, 14, 233, 0);
 }
 
 .user-message {
@@ -85,12 +96,86 @@ nav .navlink:hover {
 }
 
 #app {
-  background-image:
-    url("../img/landscape-mountains.jpg");
+  background-image: url("../img/landscape-mountains.jpg");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
   min-width: 100vw;
   min-height: 100vh;
+}
+
+.hamburger {
+  background: white;
+  min-height: 5px;
+  height: 5px;
+  transition: 1s;
+  width: 40px;
+  border-radius: 4px;
+}
+
+.hamburger::before,
+.hamburger::after {
+  height: 5px;
+  background: white;
+  content: "";
+  display: block;
+  position: relative;
+  transition: 1s;
+  border-radius: 4px;
+}
+
+.hamburger::before {
+  top: -12px;
+}
+
+.hamburger::after {
+  top: 7px;
+}
+
+.icon {
+  height: 50px;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5vw;
+}
+
+.icon input {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  cursor: pointer;
+  opacity: 0;
+  z-index: 9999999;
+}
+
+input:checked ~ .hamburger {
+  background: transparent;
+}
+
+input:checked ~ .hamburger::before {
+  transform: rotate(45deg);
+  top: 0;
+}
+
+input:checked ~ .hamburger::after {
+  transform: rotate(-45deg);
+  top: -5px;
+}
+
+@media (max-width: 600px) {
+  nav, .navbar {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+  }
+
+  nav .navlink {
+    width: 100vw;
+    background-color: green;
+  }
+
+  
 }
 </style>
